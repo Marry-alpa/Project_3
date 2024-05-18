0d13af1609f08515ad33a470cfd57b6ab03cf4c3
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2024-05-18 21:00:56 +0300

    Update Amphib.java

--- src/main/java/com/example/demo3/part_1_3/amphib/Amphib.java
+++ src/main/java/com/example/demo3/part_1_3/amphib/Amphib.java
@@ -1,2 +1,8 @@
-package com.example.demo3.part_1_3.amphib;public class Amphib {
+package com.example.demo3.part_1_3.amphib;
+
+import com.example.demo3.part_1_3.SoundMaker;
+
+abstract class Amphib extends SoundMaker {
+    public abstract void makeSound();
 }
+

44cf174b38bb9c48ad9410984e9aa7299bd20438
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2024-05-18 21:01:28 +0300

    Update Animal.java

--- src/main/java/com/example/demo3/part_1_3/animals/Animal.java
+++ src/main/java/com/example/demo3/part_1_3/animals/Animal.java
@@ -1,2 +1,8 @@
-package com.example.demo3.part_1_3.animals;public class Animal {
+package com.example.demo3.part_1_3.animals;
+
+import com.example.demo3.part_1_3.SoundMaker;
+
+public abstract class Animal extends SoundMaker {
+    public abstract void makeSound();
 }
+

78d347f727e019d3e239bf294957500f9e9365d8
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2024-05-18 21:03:33 +0300

    Update Insects.java

--- src/main/java/com/example/demo3/part_1_3/insects/Insects.java
+++ src/main/java/com/example/demo3/part_1_3/insects/Insects.java
@@ -1,2 +1,7 @@
-package com.example.demo3.part_1_3.insects;public class Insects {
+package com.example.demo3.part_1_3.insects;
+
+import com.example.demo3.part_1_3.SoundMaker;
+
+abstract class Insects extends SoundMaker {
+    public abstract void makeSound();
 }

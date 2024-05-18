28d230a3519027609875eda209882b5fd69145b7
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2024-05-18 21:02:30 +0300

    Update Pig.java

--- src/main/java/com/example/demo3/part_1_3/animals/Pig.java
+++ src/main/java/com/example/demo3/part_1_3/animals/Pig.java
@@ -1,2 +1,8 @@
-package com.example.demo3.part_1_3.animals;public class Pig {
+package com.example.demo3.part_1_3.animals;
+
+class Pig extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Oink");
+    }
 }

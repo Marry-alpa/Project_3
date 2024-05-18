5e14c1c9bceab854dc7de9a9ead805f332146be4
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2024-05-18 21:02:48 +0300

    Update Bee.java

--- src/main/java/com/example/demo3/part_1_3/insects/Bee.java
+++ src/main/java/com/example/demo3/part_1_3/insects/Bee.java
@@ -1,2 +1,9 @@
-package com.example.demo3.part_1_3.insects;public class Bee {
+package com.example.demo3.part_1_3.insects;
+
+
+public class Bee extends Insects {
+    @Override
+    public void makeSound() {
+        System.out.println("Buzz");
+    }
 }

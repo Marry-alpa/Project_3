bce2f5d8a761627261d1ac3938b844ea7aa51fbb
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2024-05-18 21:04:08 +0300

    Update SoundMaker.java

--- src/main/java/com/example/demo3/part_1_3/SoundMaker.java
+++ src/main/java/com/example/demo3/part_1_3/SoundMaker.java
@@ -1,2 +1,24 @@
-package com.example.demo3.part_1_3;public class SoundMaker {
+package com.example.demo3.part_1_3;
+
+import com.example.demo3.part_1_3.amphib.*;
+import com.example.demo3.part_1_3.animals.*;
+import com.example.demo3.part_1_3.insects.*;
+
+
+public class SoundMaker {
+    public static void main(String[] args) {
+        Animals myAnimal = new Animals();
+        Dog myDog = new Dog();
+        Cat myCat = new Cat();
+        Frog myFrog = new Frog();
+        Cricket myCricket = new Cricket();
+        Bee myBee = new Bee();
+        myAnimal.makeSound(); // Some sound
+        myDog.makeSound(); // Bark
+        myCat.makeSound(); // Meow
+        myFrog.makeSound();
+        myCricket.makeSound();
+        myBee.makeSound();
+    }
 }
+

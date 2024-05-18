5b0396def68fe8cf6ed4acfa924c01c4e805d862
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2024-05-18 21:04:55 +0300

    Update OuterClassWithLocal.java

--- src/main/java/com/example/demo3/part_2/OuterClassWithLocal.java
+++ src/main/java/com/example/demo3/part_2/OuterClassWithLocal.java
@@ -1,3 +1,23 @@
 package com.example.demo3.part_2;
 
+public class OuterClassWithLocal {
+    void someMethod() {
+        final String localVariable = "Local variable";
+
+        // Local class
+        class LocalClass {
+            void display() {
+                System.out.println("Local class accessing: " + localVariable);
+            }
+        }
+
+        LocalClass local = new LocalClass();
+        local.display();
+    }
+
+    public static void main(String[] args) {
+        OuterClassWithLocal outer = new OuterClassWithLocal();
+        outer.someMethod();
+    }
+}
 

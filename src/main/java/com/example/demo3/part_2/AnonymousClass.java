5b742b2ea9bc23a1515e9098039514ea32c65149
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2024-05-18 21:04:38 +0300

    Update AnonymousClass.java

--- src/main/java/com/example/demo3/part_2/AnonymousClass.java
+++ src/main/java/com/example/demo3/part_2/AnonymousClass.java
@@ -1,2 +1,26 @@
-package com.example.demo3.part_2;public class AnonymousClass {
-}
+package com.example.demo3.part_2;
+
+    public class AnonymousClass {
+
+        interface Greeting {
+            void greet();
+        }
+
+        void createAnonymousClass() {
+            // Anonymous class implementing the Greeting interface
+            Greeting greeting = new Greeting() {
+                @Override
+                public void greet() {
+                    System.out.println("Hello from anonymous class!");
+                }
+            };
+
+            greeting.greet();
+        }
+
+        public static void main(String[] args) {
+            AnonymousClass demo = new AnonymousClass();
+            demo.createAnonymousClass();
+        }
+    }
+

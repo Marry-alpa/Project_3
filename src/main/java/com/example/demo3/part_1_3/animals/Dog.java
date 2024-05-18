cca7b50440fb5fd837e2ff0afbf0ec580e3fc22a
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2024-05-18 21:02:15 +0300

    Update Dog.java

--- src/main/java/com/example/demo3/part_1_3/animals/Dog.java
+++ src/main/java/com/example/demo3/part_1_3/animals/Dog.java
@@ -1,2 +1,8 @@
-package com.example.demo3.part_1_3.animals;public class Dog {
+package com.example.demo3.part_1_3.animals;
+
+public class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Woof");
+    }
 }

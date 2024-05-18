3e04472d4c42a3a86e480404813f3cc5bf91f436
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2024-05-18 21:03:04 +0300

    Update Cricket.java

--- src/main/java/com/example/demo3/part_1_3/insects/Cricket.java
+++ src/main/java/com/example/demo3/part_1_3/insects/Cricket.java
@@ -1,2 +1,8 @@
-package com.example.demo3.part_1_3.insects;public class Cricket {
+package com.example.demo3.part_1_3.insects;
+
+public class Cricket extends Insects {
+    @Override
+    public void makeSound() {
+        System.out.println("Chrip");
+    }
 }
